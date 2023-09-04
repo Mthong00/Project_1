86ff9045fbb72e6091c745b589705a2bb4cc22f3
Mthong00 <mthong0810@gmail.com>
2023-09-04 18:53:19 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!!");
+        Console.WriteLine("Nhập tên:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Nhập nơi sinh:");
+        string placeofbirth = Console.ReadLine();
+        Console.WriteLine("Nhập ngày sinh:");
+        string dateofbirth = Console.ReadLine();
+        Console.WriteLine("Nhập tuổi:")
+            ;
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Thông tin:" + name + " - " + placeofbirth + " - " + dateofbirth + " - " + age);
+    }
+}
